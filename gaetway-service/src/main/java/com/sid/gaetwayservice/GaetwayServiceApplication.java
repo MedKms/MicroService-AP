91a3ca318b08b5942a76c33402d8613295d2d8a1
MedKms <mohamed.kemmous-etu@etu.univh2c.ma>
2021-10-24 22:43:00 +0200

    add gateway-service

---
+++ gaetway-service/src/main/java/com/sid/gaetwayservice/GaetwayServiceApplication.java
@@ -0,0 +1,22 @@
+package com.sid.gaetwayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GaetwayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaetwayServiceApplication.class, args);
+    }
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient rdc
+            , DiscoveryLocatorProperties properties){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
+    }
+}
---
+++ gaetway-service/src/test/java/com/sid/gaetwayservice/GaetwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.gaetwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaetwayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
